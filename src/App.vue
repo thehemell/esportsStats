1de85999fc95b9c85f93625617534244323472b3
Danil Rayanov <d.rayanov@esforce.com>
2019-12-20 18:16:34 +0300

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,136 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <div class="section__header">
+        <h2>Matches</h2>
+      </div>
+      <div class="section__content">
+        <Matches v-for="(item) in matchesList" v-bind:match="item" v-bind:key="item.id"/>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Matches from './components/Matches'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Matches
+  },
+  data () {
+    return {
+      matchesList: [
+        {
+          id: 1,
+          team1: {
+            name: 'Vitality',
+            logo: 'https://static.hltv.org/images/team/logo/9565',
+            score: 2,
+          },
+          team2: {
+            name: 'ForZe',
+            logo: 'https://static.hltv.org/images/team/logo/8135',
+            score: 1,
+          },
+        },
+        {
+          id: 2,
+          team1: {
+            name: 'Na`Vi',
+            logo: 'https://static.hltv.org/images/team/logo/4608',
+            score: 2,
+          },
+          team2: {
+            name: 'mousesports',
+            logo: 'https://static.hltv.org/images/team/logo/4494',
+            score: 1,
+          },
+        },
+        {
+          id: 3,
+          team1: {
+            name: 'Heroic',
+            logo: 'https://static.hltv.org/images/team/logo/7175',
+            score: 0
+          },
+          team2: {
+            name: 'Virtus.pro',
+            logo: 'https://static.hltv.org/images/team/logo/5378',
+            score: 2
+          },
+        },
+        {
+          id: 4,
+          team1: {
+            name: 'Heroic',
+            logo: 'https://static.hltv.org/images/team/logo/7175',
+            score: '-',
+          },
+          team2: {
+            name: 'Virtus.pro',
+            logo: 'https://static.hltv.org/images/team/logo/5378',
+            score: '-',
+          },
+        },
+        {
+          id: 5,
+          team1: {
+            name: 'Vitality',
+            logo: 'https://static.hltv.org/images/team/logo/9565',
+            score: '-',
+          },
+          team2: {
+            name: 'ForZe',
+            logo: 'https://static.hltv.org/images/team/logo/8135',
+            score: '-',
+          },
+        },
+        {
+          id: 6,
+          team1: {
+            name: 'Na`Vi',
+            logo: 'https://static.hltv.org/images/team/logo/4608',
+            score: '-',
+          },
+          team2: {
+            name: 'mousesports',
+            logo: 'https://static.hltv.org/images/team/logo/4494',
+            score: '-',
+          },
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #1b1f23;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 1280px;
+}
+
+.section__header {
+  display: flex;
+  color: white;
+}
+
+.section__content {
+  display: grid;
+  flex-wrap: wrap;
+  grid-gap: 12px;
+  grid-template-columns: repeat(3, 1fr);
 }
 </style>
---
+++ src/components/Matches.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="c-match">
+        <div class="c-match__header">
+            <div>14:55</div>
+            <div>EPICENTER 2019</div>
+        </div>
+        <div class="c-match__content">
+            <div class="c-match__team c-match__team_first">
+                <div class="c-match__team-name">{{ match.team1.name }}</div>
+                <img class="c-match__team-logo" :src="match.team1.logo" @click="openImage($event)">
+            </div>
+            <div class="c-match__score">{{ match.team1.score }} : {{ match.team2.score }}</div>
+            <div class="c-match__team c-match__team_second">
+                <div class="c-match__team-name">{{ match.team2.name }}</div>
+                <img class="c-match__team-logo" v-bind:src="match.team2.logo">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Matches",
+        props: {
+            match: Object
+        },
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+            openImage(event) {
+                console.log(event.target);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    * {
+        box-sizing: border-box;
+    }
+
+    .c-match {
+        background-color: #232d38;
+        border-radius: 6px;
+        color: #b9bdbf;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        padding: 10px;
+        font-size: 14px;
+    }
+
+    .c-match__header {
+        display: flex;
+        justify-content: space-between;
+        font-weight: 800;
+    }
+
+    .c-match__content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 80px;
+    }
+
+    .c-match__team {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        font-size: 18px;
+        font-weight: 800;
+    }
+
+    .c-match__team_first {
+        padding-right: 35px;
+        justify-content: flex-end;
+    }
+
+    .c-match__team_second {
+        padding-left: 35px;
+        justify-content: flex-start;
+    }
+
+    .c-match__team-logo {
+        width: 48px;
+        height: 48px;
+    }
+
+    .c-match__team_first > .c-match__team-logo {
+        margin-left: 8px;
+    }
+
+    .c-match__team_second > .c-match__team-logo {
+        margin-right: 8px;
+        order: 0;
+    }
+
+    .c-match__team_second > .c-match__team-name {
+        order: 1;
+    }
+
+    .c-match__score {
+        position: absolute;
+        margin: 0 auto;
+        width: 70px;
+        font-size: 22px;
+        font-weight: 900;
+    }
+</style>
